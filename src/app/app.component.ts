ae5d34c4172efa99d4df4be47873a22db32e481d
qwertasdfzxcvj <145374479+Jessenia111@users.noreply.github.com>
2025-07-04 14:18:43 +0300

    added horizontal scrolling

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],  
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+@ViewChild('slider', {static:false }) slider!: ElementRef;
+
+  scrollRight(): void {
+    console.log("clicked");
+    this.slider.nativeElement.scrollLeft += 150;
+  }
+
+}
